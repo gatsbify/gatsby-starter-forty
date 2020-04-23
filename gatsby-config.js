3a34da34867adad869268a259026adc797efc534
botmasterza <flo@gatsbify.site>
2020-04-23 14:59:08 +0200

    1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Forty V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Forty by HTML5 UP"
+    title: 'Gatsbify - Website Conversion Service',
+    author: '@gatsbify.team',
+    description: 'Gatsby Website converter',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    `gatsby-plugin-preact`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -19,6 +20,6 @@ module.exports = {
       },
     },
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
+    'gatsby-plugin-offline',
   ],
 }
